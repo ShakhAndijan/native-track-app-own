94190691a160023648de42bd5997d344aa2ba094
ShakhBek <shakhandijan@gmail.com>
2025-12-30 10:08:51 +0500

    server and client

---
+++ tracks/src/screens/AccountScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+const AccountScreen = () => {
+  return <Text style={{ fontSize: 50 }}>AccountScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default AccountScreen;
---
+++ tracks/src/screens/TrackListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+const TrackListScreen = () => {
+  return <Text style={{ fontSize: 50 }}>TrackListScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackListScreen;
